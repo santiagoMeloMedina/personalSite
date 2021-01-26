50e0ec7c0f5021e6fe18e2d53657243efa1a0d0f
Santiago Melo Medina <smelomedina05@gmail.com>
2021-01-26 17:33:23 -0500

    email page almost design funtionality done

--- pages/email.js
+++ pages/email.js
@@ -1,32 +1,45 @@
 
 import { Grid, Button } from '@material-ui/core';
 import styles from '../styles/email.module.css';
+import cn from 'classnames';
+import { useState } from 'react';
+
+function checkValidity(state, setState) {
+    let result = state.name != "" && state.email != "" && state.message != "";
+    setState({ ...state, send: result });
+}
+
+function updateTextValue(state, setState, key, value) {
+    let newInputs = state.inputs;
+    newInputs[key] = value;
+    setState({ ...state, inputs: newInputs });
+    checkValidity();
+}
 
 export default function email() {
+    const [state, setState] = useState({ send: false, inputs: { name: "", email: "", message: ""}});
     return (
-        <Grid container className={styles.container} xs={12} sm={12} direction="row">
+        <Grid container className={styles.container} xs={12} sm={12} direction="row" alignItems="center" justify="center">
             <Grid className={styles.header} xs={12} sm={12}>
             </Grid>
-            <Grid container className={styles.field} xs={12} sm={12}>
-                <Grid container  alignItems="center" justify="center">
-                    <Button
-                        onClick={()=>{}}
-                    >
-                        send mail
-                    </Button>
-                </Grid>
-                <Grid container alignItems="center" justify="center" direction="column">
+            <Grid container className={styles.field} direction="column" alignItems="center" justify="center" xs={9} sm={9}>
+                <Grid container className={styles.form} direction="column" alignItems="center" justify="center">
                     <input className={styles.text} 
-                        placeholder="full name"
-                        justify="center"
+                        placeholder="full name" 
+                        onChange={(e)=>{ updateTextValue(state, setState, "name", e.target.value) }}
                     ></input>
                     <input className={styles.text} 
                         placeholder="email"
-                        justify="center"
+                        onChange={(e)=>{ updateTextValue(state, setState, "email", e.target.value) }}
                     ></input>
                     <textarea className={styles.message} 
                         placeholder="message"
+                        onChange={(e)=>{ updateTextValue(state, setState, "message", e.target.value) }}
                     ></textarea>
+                    <button className={cn({
+                        [styles.send]: !state.send,
+                        [styles.sendactive]: state.send
+                    })}>send</button>
                 </Grid>
             </Grid>
             <Grid container className={styles.foot} xs={12} sm={12}>
