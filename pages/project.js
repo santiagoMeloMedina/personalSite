972c3ac6dacd9feea5defc71e89c43de69594c86
Santiago Melo Medina <smelomedina05@gmail.com>
2021-01-26 13:51:23 -0500

    added unvanish effect to projects page

--- pages/project.js
+++ pages/project.js
@@ -1,6 +1,8 @@
 
+import styles from '../styles/project.module.css';
+
 export default function project() {
     return (
-        <div>project</div>
+        <div className={styles.container}>project</div>
     );
 }
