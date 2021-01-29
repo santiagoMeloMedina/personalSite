67e78e46999950d275b5bd2fbcb070384279ac76
Santiago Melo Medina <smelomedina05@gmail.com>
2021-01-29 17:27:33 -0500

    adding template to email

--- back/main/mail/services.py
+++ back/main/mail/services.py
@@ -1,13 +1,16 @@
 
 from django.core.mail import send_mail
 from .constants import EMAIL_SENDER, EMAIL_SUBJECT
+import os
 
 def sendEmail(name, email, message, **kwargs):
-    message = "{} -- {}".format(name, message)
+    file = open(os.path.join(os.path.dirname(__file__), 'resources/email_template.html'), 'r')
+    message = ''.join(file.readlines()).replace('@@email@@', email).replace('@@message@@', message)
     res = send_mail(
-        subject = EMAIL_SUBJECT,
+        subject = name,
         message = message,
         from_email = email, 
         recipient_list = [EMAIL_SENDER]
     )
+    file.close()
     return res
